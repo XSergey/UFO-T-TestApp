fcf4680812267e436fd736a832155547e0d06ccb
XSergey <sergey.martishin.inbox@gmail.com>
2017-05-10 16:55:35 +0300

    Fix run error
    Fix run error

--- public/bootstrap.php
+++ public/bootstrap.php
@@ -4,10 +4,6 @@
  */
 require __DIR__.'/../vendor/autoload.php';
 
-include(__DIR__.'/../src/Illuminati/Container/Operations.php');
-include(__DIR__.'/../src/Illuminati/Container/InsertOperation.php');
-include(__DIR__.'/../src/Illuminati/Container/DeleteOperation.php');
-include(__DIR__.'/../src/Illuminati/Container/ReplaceOperation.php');
 include(__DIR__.'/../src/Illuminati/Container/IlluminatiService.php');
 include(__DIR__.'/../src/Illuminati/Container/IlluminatiServiceProvider.php');
 
--- src/Illuminati/Container/IlluminatiServiceProvider.php
+++ src/Illuminati/Container/IlluminatiServiceProvider.php
@@ -3,44 +3,44 @@ namespace Sergey\Illuminati;
 
 class IlluminatiServiceProvider extends IlluminatiService{
 
-    public function __construct(array $files) {
-        $this->run($files);
-    }
+	public function __construct(array $files) {
+		$this->run($files);
+	}
 
-    public static function compareFiles(array $files, $html = false){
-        $source = new IlluminatiServiceProvider($files);
-        
-        if($html === true)
-		    return $source->getHtml();
-        else
-            return $source->getStatuses();
-    }
+	public static function compareFiles(array $files, $html = false){
+		$source = new IlluminatiServiceProvider($files);
+		
+		if($html === true)
+			return $source->getHtml();
+		else
+			return $source->getStatuses();
+	}
 
-    protected function run($files){
-        if(count($files) < 2) {
-            // need more than one file
-            return;
-        } else {
-		    $this->__processStack($files);
+	protected function run($files){
+		if(count($files) < 2) {
+			// need more than one file
+			return;
+		} else {
+			$this->__processStack($files);
 		}
-    }
+	}
 
-    protected function getHtml(){
-        $html = '<html><head></head><body>';
-        $html .= '<table>';
-        $html .= '<thead>';
-        $html .= '</thead>';
-        $html .= '<tbody>';
-        foreach($this->difference as $d=>$diff)
-        {            
-            $html .= '<tr>';
-            $html .= '<td>'.$d.'</td>';
-            $html .= '<td>'.$diff['status'].'</td>';
-            $html .= '<td>'.$diff['result'].'</td>';
-            $html .= '</tr>';
-        }
-        $html .= '</tbody>';
-        $html .= '</table>';
-        echo $html;
-    }
+	protected function getHtml(){
+		$html = '<html><head></head><body>';
+		$html .= '<table>';
+		$html .= '<thead>';
+		$html .= '</thead>';
+		$html .= '<tbody>';
+		foreach($this->difference as $d=>$diff)
+		{            
+			$html .= '<tr>';
+			$html .= '<td>'.$d.'</td>';
+			$html .= '<td>'.$diff['status'].'</td>';
+			$html .= '<td>'.$diff['result'].'</td>';
+			$html .= '</tr>';
+		}
+		$html .= '</tbody>';
+		$html .= '</table>';
+		echo $html;
+	}
 }
